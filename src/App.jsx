ec979a0296e5f5b7ce958eddb7f87dc0855bd7b3
Gustavo Oliveira <gusta.oli.fernandes@gmail.com>
2026-05-01 04:22:03 -0300

    fix: logo maior cortando áreas transparentes com overflow hidden

--- src/App.jsx
+++ src/App.jsx
@@ -166,7 +166,7 @@ export default function App() {
 
       <aside className="sidebar">
         <div className="sidebar-logo">
-          <img src={logo} alt="CortexLab" style={{ height: '100%', width: 'auto', maxWidth: '100%', objectFit: 'contain', padding: '10px 0' }} />
+          <img src={logo} alt="CortexLab" />
         </div>
 
         <nav className="sidebar-nav">
